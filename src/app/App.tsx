a9e9658540f74952ee139558fedd6939d58982b9
Itay <ibasoni@gmail.com>
2020-06-18 06:36:58 +0300

    Move loadUsers effect to  App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { loadAllUsers } from "../users/users-actions";
 import UsersList from "../users/UsersList";
 import { handleSelectUser } from "./app-actions";
 import "./App.css";
 import PostsListContainer from "./containers/PostsListContainer";
 
 function App() {
+  useEffect(() => {
+    loadAllUsers();
+  }, []);
+
   return (
     <div className="App">
       <UsersList onSelect={handleSelectUser} />
--- src/users/users-actions.ts
+++ src/users/users-actions.ts
@@ -1,4 +1,13 @@
+import { fetchUsers } from "./users-api";
 import { UsersStore } from "./users-store";
+
+export const loadAllUsers = () => {
+  fetchUsers().then((users) => {
+    UsersStore.update((s) => {
+      s.users = users;
+    });
+  });
+};
 
 export const setCurrentUser = (userId: number) => {
   // set selected user
